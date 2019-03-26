166c959f186783c30046ffdb7f0f0ec36095a4f5
Ajay Pratap <ajaprata@publicisgroupe.net>
2019-03-26 11:46:50 +0530

    fixed content negotiation issue

--- BookStore/src/main/java/com/store/configuration/ContentNegotiationConfiguration.java
+++ BookStore/src/main/java/com/store/configuration/ContentNegotiationConfiguration.java
@@ -3,19 +3,16 @@ package com.store.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableWebMvc
-public class RestApiApplication extends WebMvcConfigurationSupport {
+public class ContentNegotiationConfiguration extends WebMvcConfigurationSupport {
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-		configurer
-//		.favorPathExtension(false)
-////		favorParameter(true).ignoreAcceptHeader(false)
-				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+		configurer.ignoreAcceptHeader(false).favorParameter(false).defaultContentType(MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML).mediaType("json", MediaType.APPLICATION_JSON);
+
 	}
 }
